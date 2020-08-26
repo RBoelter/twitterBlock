c99ee36121b22b40c0238949dd41135d406c08f8
RBoelter <ronny.boelter@outlook.com>
2020-08-26 15:02:48 +0200

    merge upstream

--- cypress/tests/functional/Twitterblock.spec.js
+++ cypress/tests/functional/Twitterblock.spec.js
@@ -2,48 +2,35 @@ describe('Twitter Block plugin tests', function () {
 
 	it('Disable Twitter Block', function () {
 		cy.login('admin', 'admin', 'publicknowledge');
-
 		cy.get('ul[id="navigationPrimary"] a:contains("Settings")').click();
 		cy.get('ul[id="navigationPrimary"] a:contains("Website")').click();
 		cy.get('button[id="plugins-button"]').click();
-
 		// disable plugin if enabled
-		cy.get('input[id^="select-cell-twitterblockplugin-enabled"]').then($btn => {
-			if ($btn.attr('checked') === 'checked') {
-				cy.get('input[id^="select-cell-twitterblockplugin-enabled"]').click();
-				cy.get(
-					'div[class*="pkp_modal_panel"] button[class*="pkpModalConfirmButton"]'
-				).click();
-				cy.get(
-					'div:contains(\'The plugin "Twitter Block" has been disabled.\')'
-				);
-			}
-		});
+		cy.get('input[id^="select-cell-twitterblockplugin-enabled"]')
+			.then($btn => {
+				if ($btn.attr('checked') === 'checked') {
+					cy.get('input[id^="select-cell-twitterblockplugin-enabled"]').click();
+					cy.get('div[class*="pkp_modal_panel"] button[class*="pkpModalConfirmButton"]').click();
+					cy.get('div:contains(\'The plugin "Twitter Block" has been disabled.\')');
+				}
+			});
 	});
 
 	it('Enable Twitter Block', function () {
 		cy.login('admin', 'admin', 'publicknowledge');
-
 		cy.get('ul[id="navigationPrimary"] a:contains("Settings")').click();
 		cy.get('ul[id="navigationPrimary"] a:contains("Website")').click();
 		cy.get('button[id="plugins-button"]').click();
-
 		// Find and enable the plugin
 		cy.get('input[id^="select-cell-twitterblockplugin-enabled"]').click();
 		cy.get('div:contains(\'The plugin "Twitter Block" has been enabled.\')');
 		cy.waitJQuery();
-		cy.get(
-			'tr[id="component-grid-settings-plugins-settingsplugingrid-category-blocks-row-twitterblockplugin"] a[class="show_extras"]'
-		).click();
-		cy.get(
-			'a[id^="component-grid-settings-plugins-settingsplugingrid-category-blocks-row-twitterblockplugin-settings-button"]'
-		).click();
+		cy.get('tr[id="component-grid-settings-plugins-settingsplugingrid-category-blocks-row-twitterblockplugin"] a[class="show_extras"]').click();
+		cy.get('a[id^="component-grid-settings-plugins-settingsplugingrid-category-blocks-row-twitterblockplugin-settings-button"]').click();
 		// Fill out settings form
 		cy.get('form[id="twitterSettings"] input[name="tweetTitle"]')
 			.clear()
-			.type(
-				'Tweets by <a href="https://twitter.com/JohnWillinsky" target="_blank">@JohnWillinsky</a>'
-			);
+			.type('Tweets by <a href="https://twitter.com/JohnWillinsky" target="_blank">@JohnWillinsky</a>');
 		cy.get('form[id="twitterSettings"] input[name="tweetUrl"]')
 			.clear()
 			.type('https://twitter.com/JohnWillinsky');
@@ -62,23 +49,17 @@ describe('Twitter Block plugin tests', function () {
 		// submit settings form
 		cy.get('form[id="twitterSettings"] button[id^="submitFormButton"]').click();
 		cy.waitJQuery();
-
 		// enable block in sidebar if disabled
 		cy.get('ul[id="navigationPrimary"] a:contains("Settings")').click();
 		cy.get('ul[id="navigationPrimary"] a:contains("Website")').click();
 		cy.get('div[class*="pkpTabs--side"] button[id="setup-button"]').click();
-		cy.get(
-			'div[class*="pkpTabs--side"] #setup input[value="twitterblockplugin"]'
-		).then($btn => {
-			if ($btn.attr('checked') !== 'checked' && $btn.attr('checked') !== true) {
-				cy.get(
-					'div[class*="pkpTabs--side"] #setup input[value="twitterblockplugin"]'
-				).check();
-				cy.get(
-					'div[class*="pkpTabs--side"] #setup div[class="pkpFormPage__buttons"] button[class="pkpButton"]'
-				).click();
-			}
-		});
+		cy.get('div[class*="pkpTabs--side"] #setup input[value="twitterblockplugin"]')
+			.then($btn => {
+				if ($btn.attr('checked') !== 'checked' && $btn.attr('checked') !== true) {
+					cy.get('div[class*="pkpTabs--side"] #setup input[value="twitterblockplugin"]').check();
+					cy.get('div[class*="pkpTabs--side"] #setup div[class="pkpFormPage__buttons"] button[class="pkpButton"]').click();
+				}
+			});
 		// go to journal index
 		cy.get('ul[id="navigationUser"] li[class="view_frontend"] a').click();
 		cy.get('div[class*="block_twitter"]');
